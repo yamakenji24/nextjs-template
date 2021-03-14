a2e95f1c0ff058911ee995a925bd78cfb3221216
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-03-15 01:31:07 +0900

    template

---
+++ next.config.js
@@ -0,0 +1,11 @@
+import path from 'path'
+
+module.exports = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      src: path.join(__dirname, 'src/'),
+    }
+    return config
+  },
+}
